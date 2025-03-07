3954e07ba13034a6516fd57c274f9fcacad9b5a4
GAURAV-KUMAR22 <gorav.panwar99@gmail.com>
2025-03-07 17:16:25 +0530

    mongodb update

--- Backend/Db/Db.mongo.js
+++ Backend/Db/Db.mongo.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
 export default function DbConnection() {
-    mongoose.connect(process.env.MONGODB_URI,{  useNewUrlParser: true,
-    useUnifiedTopology: true}).then((success) => console.log('Db connected Succussfully')).catch((error) => console.log(error.message));
+    mongoose.connect(process.env.MONGODB_URI).then((success) => console.log('Db connected Succussfully')).catch((error) => console.log(error.message));
 };
